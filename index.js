d9685f0f580908c20d6624f860eacbd09cd18dc1
Hadi <hadichalabi@gmail.com>
2014-10-28 14:53:06 +0000

    added connect and disconnect messages

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.get('/', function(request, response){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+  socket.broadcast.emit('user connected notification', 'user connected');
+  socket.on('chat message', function(msg){ 
     io.emit('chat message', msg);
   });
+  socket.on('disconnect', function () {
+    console.log('disconnected');
+    io.sockets.emit('user disconnected notification', 'user disconnected');
+  });
 });
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
